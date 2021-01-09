1ca2f26b2098b794c76fd3298f403380a32249fd
Khole Jones <khole.jones@studio.co.uk>
2021-01-09 12:14:50 +0000

    Bump pyhiveapi==0.3.4.1 and light fix

--- homeassistant/components/hive/__init__.py
+++ homeassistant/components/hive/__init__.py
@@ -84,7 +84,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: config_entries.ConfigEnt
             _LOGGER.error("Cannot boost entity id entered")
             return
 
-        device = hive.session.helper.get_device_from_id(node_id)
+        device = hive.session.helper.getDeviceFromID(node_id)
         minutes = service_call.data[ATTR_TIME_PERIOD]
         temperature = service_call.data[ATTR_TEMPERATURE]
 
@@ -98,7 +98,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: config_entries.ConfigEnt
             _LOGGER.error("Cannot boost entity id entered")
             return
 
-        device = hive.session.helper.get_device_from_id(node_id)
+        device = hive.session.helper.getDeviceFromID(node_id)
         minutes = service_call.data[ATTR_TIME_PERIOD]
         mode = service_call.data[ATTR_MODE]
 
